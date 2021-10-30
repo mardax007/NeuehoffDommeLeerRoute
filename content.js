5d74a2719fa066eec8f8fed3de64ed63936c794e
mardax007 <62964422+mardax007@users.noreply.github.com>
2021-10-30 19:47:27 +0200

    Update

--- content.js
+++ content.js
@@ -1,12 +1,10 @@
 function myTimer() {
-    setTimeout(function(){
-        if(document.getElementsByClassName("disabled").length != 0)
-        {
-            for (var i=0, max=(document.getElementsByClassName("disabled").length); i < max; i++) {
-                document.getElementsByClassName("disabled")[i].style.pointerEvents = "all"
-            }
+    if(document.getElementsByClassName("disabled").length != 0)
+    {
+        for (var i=0, max=(document.getElementsByClassName("disabled").length); i < max; i++) {
+            document.getElementsByClassName("disabled")[0].classList.remove("disabled");
         }
-    }, 5000);
+    }
 }
 
-var myVar = setInterval(myTimer, 5000);
+var x = setInterval(myTimer, 1000);
